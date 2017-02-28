92bdacc68b7bb0b49386cc0b39f9917c92295427
Harmiox <Harmiox@gmail.com>
2017-02-28 13:05:52 -0500

    Update index.js

--- cogs/index.js
+++ cogs/index.js
@@ -1,58 +1,120 @@
-//Global Functions
-function readGivenFile(theFile) {
-    fs = require('fs');
-    let data = fs.readFileSync(theFile).toString();
-    return data;
-}
-function writeGivenFile(theFile, fileData) {
-    var fs = require('fs');
-    fs.writeFileSync(theFile, fileData);
-}
+const db = require('sqlite');
+db.open('./cogs/databases.db');
+console.log("index.js loaded");
 
-//Cog Loader
 var loadedCogs = {};
+var unloadedCogs = {};
 function loadCogs() {
-    loadedCogs = {};
-    let importedCogs = JSON.parse(readGivenFile("./cogs.json"));
+    //require()
     require('fs').readdirSync(__dirname + '/').forEach(function(file) {
       if (file.match(/\.js$/) !== null && file !== 'index.js') {
         var name = file.replace('.js', '');
-        exports[name] = require('./' + file);
+        unloadedCogs[name] = require('./' + file);
       }
     });
-    var count = 0;
-    for (var obj in importedCogs.cogs) {
-        if(importedCogs.cogs.hasOwnProperty(obj) ) {
-            let cog = obj.replace('.js', '')
-            if (!exports[cog]) {
-                //console.log(cog + ".js | imported = false");
+    //instances
+    db.all(`SELECT * FROM cogs`).then(rows => {
+        for (let row in rows) {
+            let name = rows[row].cog.toString().replace('.js', '');
+            if (!unloadedCogs[name]) {
                 return;
-            } else {
-                loadedCogs[cog] = new exports[cog]();
-                //console.log(cog + ".js | imported = true");
-                count += 1;
             }
-        } 
-    }
+            loadedCogs[name] = new unloadedCogs[name]();
+            //Load db into instance
+            loadedCogs[name].load(db);
+        }
+    }).catch((err) => {
+        console.log(err);
+        db.run('CREATE TABLE IF NOT EXISTS cogs (cog TEXT)').then(() => {
+            db.run('INSERT INTO cogs (cog) VALUES (?)', ["permissions.js"]);
+            console.log("table created");
+            db.get(`SELECT * FROM cogs`).then(rows => {
+                for (var row in rows) {
+                    let name = rows[row].cog.toString().replace('.js', '');
+                    if (!unloadedCogs[name]) {
+                        return;
+                    }
+                    loadedCogs[name] = new unloadedCogs[name]();
+                    loadedCogs[name].load(db);
+                }
+            });
+        });
+    });
 }
-loadCogs();
-
+function addGuild(guild) {
+    db.get(`SELECT * FROM settings WHERE guildId ='${guild.id}' AND cog ='${"cogs"}'`).then(row => {
+        if (!row) {
+            db.run('INSERT INTO settings (guildId, cog, json) VALUES (?, ?, ?)', [guild.id, "cogs", getDefaultSettings()]);
+            console.log("guild added");
+        } else {
+            console.log("guild already exists");
+        }
+    });
+}
+function getDefaultSettings() {
+    settings = {
+        "permissions.js":true
+    }
+    return JSON.stringify(settings,null,"\t");
+}
 //index.js
 var cogs = function() {
     var self = this;
-    self.onMsg = function(msg) {
-        let prefix = "!";
-    }
-    self.unloaded = exports;
+    self.unloaded = unloadedCogs;
     self.loaded = loadedCogs;
-    self.reload = function() {
+    self.load = function() {
         loadCogs();
+    };
+    self.add = function(guild) {
+        addGuild(guild);
     }
-    self.readFile = function(file) {
-        return readGivenFile(file);
+    self.check = function(cog,guild) {
+        db.get(`SELECT * FROM settings WHERE guildId ='${guild.id}' AND cog ='${"cogs"}'`).then(row => {
+            if (!row) {
+                db.run('INSERT INTO settings (guildId, cog, json) VALUES (?, ?, ?)', [guild.id, "cogs", getDefaultSettings()]);
+                return false;
+            } else {
+                cogsJson = JSON.parse(row.json.toString());
+                if (cogsJson.has(cog)) {
+                    return true;
+                }
+                return false;
+            }
+        });
     }
-    self.writeFile = function(file,data) {
-        writeGivenFile(file,data);
+    self.import = function(cog,msg) {
+        db.run('INSERT INTO cogs (cog) VALUES (?)', [cog]).then(() => {
+            msg.channel.sendMessage(cog + " was imported!");
+        });
     }
-}
+    self.enable = function(cog,msg) {
+        db.get(`SELECT * FROM settings WHERE guildId ='${msg.guild.id}' AND cog ='${"cogs"}'`).then(row => {
+            if (!row) {
+                db.run('INSERT INTO settings (guildId, cog, json) VALUES (?, ?, ?)', [msg.guild.id, "cogs", getDefaultSettings()]);
+                cogsJson = getDefaultSettings();
+                cogsJson[cog] = true;
+                db.run('INSERT INTO settings (guildId, cog, json) VALUES (?, ?, ?)', [msg.guild.id, "cogs", JSON.stringify(cogsJson,null,"\t")]);
+                msg.channel.sendMessage(cog + " has been enabled!");
+            } else {
+                cogsJson = JSON.parse(row.json.toString());
+                cogsJson[cog] = true;
+                db.run(`UPDATE settings SET json = ${JSON.stringify(cogsJson,null,"\t")} WHERE guildId = ${msg.guild.id} AND cog ='${"cogs"}'`);
+                msg.channel.sendMessage(cog + " has been enabled!");
+            }
+        });
+    }
+    self.disable = function(cog,msg) {
+        db.get(`SELECT * FROM settings WHERE guildId ='${msg.guild.id}' AND cog ='${"cogs"}'`).then(row => {
+            if (!row) {
+                db.run('INSERT INTO settings (guildId, cog, json) VALUES (?, ?, ?)', [msg.guild.id, "cogs", getDefaultSettings()]);
+                msg.channel.sendMessage("No cogs installed, installed permissions.js.");
+            } else {
+                cogsJson = JSON.parse(row.json.toString());
+                delete cogsJson[cog];
+                db.run(`UPDATE settings SET json = ${JSON.stringify(cogsJson,null,"\t")} WHERE guildId = ${msg.guild.id} AND cog ='${"cogs"}'`);
+                msg.channel.sendMessage(cog + " has been disabled.");
+            }
+        });
+    }
+};
 module.exports = cogs;
