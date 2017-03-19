10f6c63ccf9f8020cdc808038103d14e119d9de7
Eric-C0 <eric.bc8@gmail.com>
2017-03-18 17:24:42 -0700

    Response

--- blackjack.js
+++ blackjack.js
@@ -1,4 +1,5 @@
 const db = require('sqlite');;
+
 function getCommands() {
     let commands = {
         "blackjack": {
@@ -23,9 +24,9 @@ function drawHand() {
 
     var num = getRandomInt(0, 13);
     if (num >= 4 || num <= 12) {
-        return (num-2) + "";
+        return (num - 2) + "";
     } else {
-        return cards[num];    
+        return cards[num];
     }
 }
 
@@ -35,19 +36,32 @@ var blackjack = function () {
         if (cmd === "!sit" || cmd === "!blackjack") {
             //Actual stuff happening
             message.channel.sendMessage("Please place your bet.");
+            //unsure how to take number in, so atm just continues if responded with "yes"
             const filter = m => (m.content.match("yes") && m.author.id === msg.author.id);
 
             msg.channel.awaitMessages(filter, {
-                max: 1,
-                time: 15000,
-                errors: ['time']
-            })
+                    max: 1,
+                    time: 15000,
+                    errors: ['time']
+                })
                 .then(function (m) {
                     //m is the messages caught by filter
                     var stringResponse = m.content;
                     var userBet = parseInt(stringResponse);
 
-                    message.channel.sendMessage("Insert hand here");
+                    var card1 = drawHand();
+                    var card2 = drawHand();
+                    message.channel.sendMessage(card1 + " " + card2);
+
+                    message.channel.sendMessage("Hit or Stay?");
+                    const filter1 = m => ((m.content.match("Hit") || m.content.match("Stay")) && m.author.id === msg.author.id);
+
+            msg.channel.awaitMessages(filter1, {
+                    max: 1,
+                    time: 15000,
+                    errors: ['time']
+                })
+
                 })
                 .catch(function () {
                     //TODO, caught
@@ -67,8 +81,7 @@ var blackjack = function () {
             let cmd = commands[command];
             db.get(`SELECT * FROM permissions WHERE guildId ='${msg.guild.id}' AND command ='${command}'`).then(row => {
                 if (!row) {
-                    db.run('INSERT INTO permissions (guildId, command, description, children, sends, alias, cooldown, rolesDenied, rolesAllowed, usersDenied, usersAllowed, channelsDenied, channelsAllowed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [msg.guild.id, command, cmd.description, cmd.children, cmd.sends, cmd.alias, cmd.cooldown, cmd.rolesDenied, cmd.rolesAllowed, cmd.usersDenied, cmd.usersAllowed, cmd.channelsDenied, cmd.channelsAllowed]).then(() => {
-                    });
+                    db.run('INSERT INTO permissions (guildId, command, description, children, sends, alias, cooldown, rolesDenied, rolesAllowed, usersDenied, usersAllowed, channelsDenied, channelsAllowed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [msg.guild.id, command, cmd.description, cmd.children, cmd.sends, cmd.alias, cmd.cooldown, cmd.rolesDenied, cmd.rolesAllowed, cmd.usersDenied, cmd.usersAllowed, cmd.channelsDenied, cmd.channelsAllowed]).then(() => {});
                 }
             });
         }
